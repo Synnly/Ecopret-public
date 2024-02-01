a0939db409027d43f25521db3b777418eca62358
Synnly <33253662+Synnly@users.noreply.github.com>
2024-02-01 10:40:39 +0100

    Update autopull.php

--- autopull.php
+++ autopull.php
@@ -5,7 +5,7 @@
         <?php
             $path = "/var/www/html/PPIL-hivemind";
 
-            echo shell_exec("cd {$path} && /usr/bin/git pull 2>&1");
+            echo shell_exec("git stash && git stash clear && cd {$path} && /usr/bin/git pull 2>&1");
         ?>
     </body>
     </html>
