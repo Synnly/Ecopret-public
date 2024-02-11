c01258750bce011c433c4f6ef74942a85b55c9ec
Synnly <emanuelfernandes54@gmail.com>
2024-02-11 21:59:34 +0100

    Ajout de tests et du bundle pour les tests

---
+++ ecopret/tests/Controller/RegistrationControllerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    use ResetDatabase, Factories;
+
+    public function testCreationCompteValide(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/register');
+
+        // Remplissage du formulaire + CGU
+        $crawler = $client->submitForm("Création du compte", ['registration_form[NomCompte]' => 'TEST',
+                                                                    'registration_form[PrenomCompte]' => 'Test',
+                                                                    'registration_form[AdresseMailCOmpte]' => 'test@test.com',
+                                                                    'registration_form[plainPassword]' => 'Testtest123',
+                                                                    'registration_form[agreeTerms]' => '1']);
+
+        // Confirmation
+        $crawler->selectButton('Création du compte');
+
+        $this->assertResponseRedirects('/main', null, "La création d'un compte valide a échoué");
+    }
+
+    public function testCreationCompteNom1a22Caracteres(): void
+    {
+        $nom = "testtesttesttesttest";
+        $NOM = "TESTTESTTESTTESTTEST";
+        $client = static::createClient();
+
+        for ($i = 1; $i <= 22; $i++) {
+
+            // Lettres minuscules
+            $client->request('GET', '/register');
+
+            $crawler = $client->submitForm("Création du compte", ['registration_form[NomCompte]' => substr($nom, 0, $i),
+                'registration_form[PrenomCompte]' => 'Test',
+                'registration_form[AdresseMailCOmpte]' => 'test@test.com',
+                'registration_form[plainPassword]' => 'Testtest123',
+                'registration_form[agreeTerms]' => '1']);
+
+            $crawler->selectButton('Création du compte');
+
+            $this->assertAnySelectorTextSame("li", "Votre nom doit commencer par une lettre majuscule puis contenir entre 1 et 19 lettres majuscules ou - .", "La création d'un compte invalide (nom = '".substr($nom, 0, $i)."', len=$i) a réussi");
+
+            // Lettres majuscules
+            $client->request('GET', '/register');
+
+            $crawler = $client->submitForm("Création du compte", ['registration_form[NomCompte]' => substr($NOM, 0, $i),
+                'registration_form[PrenomCompte]' => 'Test',
+                'registration_form[AdresseMailCOmpte]' => 'test@test.com',
+                'registration_form[plainPassword]' => 'Testtest123',
+                'registration_form[agreeTerms]' => '1']);
+
+            $crawler->selectButton('Création du compte');
+
+            if($i <= 20 and $i >= 2) {
+                $this->assertResponseRedirects('/main', null, "La création d'un compte valide a échoué (nom = '".substr($NOM, 0, $i)."', len=$i)");
+            }
+            else {
+                $this->assertAnySelectorTextSame("li", "Votre nom doit commencer par une lettre majuscule puis contenir entre 1 et 19 lettres majuscules ou - .", "La création d'un compte invalide (nom = '".substr($NOM, 0, $i)."', len=$i) a réussi");
+            }
+        }
+    }
+}
