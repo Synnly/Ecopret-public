b21cc2bd2f47dad20205500efe4a7d26705a6575
CMederic <mederic.cuny9@etu.univ-lorraine.fr>
2024-03-01 18:43:35 +0100

    début mise en ligne d'une annonce

--- ecopret/src/Controller/MainController.php
+++ ecopret/src/Controller/MainController.php
@@ -2,6 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
+use App\Entity\Compte;
+use App\Entity\Prestataire;
+use App\Entity\Utilisateur;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\AjouterAnnonceType;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,17 +17,50 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
 
         if(!$this->getUser()){
             return $this->redirectToRoute('app_login');
+        }
+        $form = $this->createForm(AjouterAnnonceType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $linkImagesForAnnouncement = "";
+            $files = [$form->get('ajouterPhoto')->getData(), $form->get('ajouterPhoto2')->getData(), $form->get('ajouterPhoto3')->getData()];
+            foreach($files as $file){
+                if($file !== null){
+                    $filename = md5(uniqid()).".png";
+                    $linkImagesForAnnouncement = $linkImagesForAnnouncement.$filename.'|';
+                    $file->move($this->getParameter('imgs_annonces'), $filename);
+                }
+            }
+            $annonce = new Annonce();
+            $annonce->setNomAnnonce($form->get("titre")->getData());
+            $annonce->setImageAnnonce($linkImagesForAnnouncement);
+            $annonce->setEstRendu(false);
+            $annonce->setEstEnLitige(false);
+            $user = $entityManager->getRepository(Utilisateur::class)->findOneBy(['noCompte' => $this->getUser()->getId()]);
+            $prestataire = $entityManager->getRepository(Prestataire::class)->findOneBy(['noUtisateur' => $user]);
+            dump($prestataire);
+            if($prestataire !== null){
+                $prestataire->setNoUtisateur($user); 
+            }else {
+                $prestataire = new Prestataire();
+                $prestataire->setNoUtisateur($user); 
+            }
+            $annonce->setPrestataire($prestataire);
+            $annonce->setDisponibilite("jamais");
+            $entityManager->persist($prestataire);
+            $entityManager->persist($annonce);
+            $entityManager->flush();               
         }
         //Page Main (il me fallait une redirection)
         //Si vous changer la route ou de fichier oublie pas de remplacer RegistrationController.php ligne 46
         return $this->render('main/index.html.twig', [
             'title' => 'EcoPrêt',
             'user' => $this->getUser(),
+            'form' => $form,
         ]);
     }
 }
---
+++ ecopret/src/Form/AjouterAnnonceType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AjouterAnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('description', TextareaType::class,[ 'attr' => [
+                'rows' => 3,
+                'cols' => 45, 
+            ],
+        ])
+            ->add('ajouterPhoto', FileType::class, [
+                'attr' => ['onchange' => "ajoutPhoto()",],
+                'required' => false,
+            ])
+            ->add('ajouterPhoto2', FileType::class, [
+                'attr' => ['onchange' => "ajoutPhoto()",],
+                'required' => false,
+            ])
+            ->add('ajouterPhoto3', FileType::class, [
+                'attr' => ['onchange' => "ajoutPhoto()",],
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
