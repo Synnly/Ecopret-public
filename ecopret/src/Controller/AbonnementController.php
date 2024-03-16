80722589e41b695acc8df521fcb7bc34da523839
rosier8u <basile.rosier6@etu.univ-lorraine>
2024-03-16 11:09:17 +0100

    Ajout d'une page abonnement pour visualiser l'abonnement si on en a un

---
+++ ecopret/src/Controller/AbonnementController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\UnsubscribeType;
+use App\Entity\Utilisateur;
+use App\Entity\Compte;
+
+
+class AbonnementController extends AbstractController
+{
+    #[Route('/subscribe', name: 'app_subscribe')]
+    public function subscribe(Request $request,EntityManagerInterface $entityManager): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $utilisateur = $entityManager->getRepository(Utilisateur::class)->findOneBy(['noCompte' => $entityManager->getRepository(Compte::class)->findOneBy(['id' => $user])]);
+
+        if($utilisateur->isPaiement()){
+            return $this->redirectToRoute('app_abonnement');
+        }
+
+        return $this->render('abonnement/subscribe.html.twig', [
+            'controller_name' => 'AbonnementController',
+        ]);
+    }
+
+    #[Route('/abonnement', name: 'app_abonnement')]
+    public function abonnement(Request $request,EntityManagerInterface $entityManager): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $utilisateur = $entityManager->getRepository(Utilisateur::class)->findOneBy(['noCompte' => $entityManager->getRepository(Compte::class)->findOneBy(['id' => $user])]);
+
+        if(!$utilisateur->isPaiement()){
+            return $this->redirectToRoute('app_subscribe');
+        }
+        
+        $form = $this->createForm(UnsubscribeType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $formData = $form->getData();
+
+            $utilisateur->setPaiement(false);
+            $utilisateur->setNbFlorains(0);
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_main');
+        }
+
+
+        return $this->render('abonnement/abonnement.html.twig', [
+            'controller_name' => 'AbonnementController',
+            'form' => $form->createView(),
+        ]);
+
+    }
+}
