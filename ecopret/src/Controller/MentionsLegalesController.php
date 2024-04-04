f881e2cde4257039f8f48c4deec22b183081658f
Synnly <emanuelfernandes54@gmail.com>
2024-04-04 08:04:02 +0000

    bugfix

--- ecopret/src/Controller/MentionsLegalesController.php
+++ ecopret/src/Controller/MentionsLegalesController.php
@@ -13,22 +13,9 @@ class MentionsLegalesController extends AbstractController
     #[Route('/mentions_legales', name: 'app_mentions_legales')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        $user = $entityManager->getRepository(Utilisateur::class)->findOneBy(['noCompte' => $this->getUser()->getId()]);
-
-        $nbNotif = 0;
-        $notifications = $this->getUser()->getNotifications();
-        
-        foreach ($notifications as $notification) {
-            if ($notification->getStatus() == 0) {
-                $nbNotif ++;
-            }
-        }
-
         return $this->render('mentions_legales/index.html.twig', [
             'controller_name' => 'MentionsLegalesController',
-            'user' => $this->getUser(),
-            'florins' => $user->getNbFlorains(),
-            'nbNotif' => $nbNotif,
+           
         ]);
     }
 }
